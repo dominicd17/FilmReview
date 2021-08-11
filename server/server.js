e705b2c0def5e2e1b721dcf402a0520b23fe92d8
dominicd17 <dominicd17@gmail.com>
2021-08-11 08:53:57 -0700

    connecting server

--- server/server.js
+++ server/server.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
 
+
+
+mongoose.connect('mongodb+srv://dom:12345@filmreview.rjutk.mongodb.net/FilmReview?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.once('open', () => {
+  console.log('Connected to Database');
+});
+
+
+
+app.get('/', (req, res) => {
+  return res.status(200).sendFile(path.join(__dirname, '../index.html'));
+});
 
 
 
 app.use('/build', express.static(path.join(__dirname, '../build')));
 
-app.get('/', (req, res) => {
-  return res.status(200).sendFile(path.join(__dirname, '../index.html'));
-});
 
 
 
